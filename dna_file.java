c8b962fa1f3868d174537b943d6c4841188e26f9
Juggernaut-77 <jetercero777@gmail.com>
2025-11-23 21:16:49 -0500

    Update dna_file.java

--- dna_file.java
+++ dna_file.java
@@ -1,3 +1,4 @@
+
 import java.io.*;
 import java.util.*;
 
@@ -7,10 +8,12 @@ public class dna_file {
 		// TODO Auto-generated method stub
 		Scanner input = new Scanner(System.in);
 		int loop = 0;
+		//creates an array list for our titles and DNA sequences
+		ArrayList<String> title = new ArrayList<String>();
 		ArrayList<String> DNA_array = new ArrayList<String>();
 		//loop to see if the file exists
 		while(loop == 0) {
-			String DNA = "";
+			String fasta = "";
 			//ask user for file to open
 			System.out.println("Type the input file: ");
 			String fname1 = input.next();
@@ -18,23 +21,34 @@ public class dna_file {
 			try {
 				File iFile = new File(fname1);
 				Scanner readF = new Scanner(iFile);
-				//count to display number of DNA sequences in file
-				int count = 1;
+				//sets current sequence to blank and current title to null
+				String cSeq = "";
+				String cTitle = null;
 				//reads from file
 				while (readF.hasNextLine()) {
-					//string goes into string DNA
-				    DNA = readF.nextLine();
-				    boolean result = DNA.matches("[ACGT]+");
-				    //if string is valid then DNA string that can turn into mRNA, if not then it will let the user know its not valid
-				    if (DNA.length() % 3 != 0 || !result) {
-				        System.out.println("[" + count + "] "+ DNA + " Is not a valid DNA string that will turn into a mRNA.");
-				    }else {
-				    	System.out.println("[" + count + "] "+ DNA + " Is a valid DNA string that will turn into a mRNA.");
-				    	//places the valid DNA strings into a array list
-				    	DNA_array.add(DNA);
+					//inputs text string into fasta
+				    fasta = readF.nextLine();
+				    //checks to see if it is a title string
+				    if(fasta.startsWith(">")) {
+				    	//if we reach the next title, save the current title and DNA sequence to the arrays
+				    	if(cTitle != null) {
+				    		title.add(cTitle);
+				    		DNA_array.add(cSeq);
+				    		//resets current DNA sequence to blank so we don't accidentally add the previous DNA sequence to the new sequence
+				    		cSeq = "";
+				    	}
+				    	//set the title to be the current title
+				    	cTitle = fasta;
+				    } else if(!fasta.isEmpty()) {
+				    	//if its not a title then we make the DNA sequence into one continuous upper case string
+				    	cSeq += fasta.toUpperCase();
 				    }
-				    count++;
 				}
+				//outside of the while loop, we add the title and DNA sequence to the list
+				if (cTitle != null) {
+			    	title.add(cTitle);
+		    		DNA_array.add(cSeq);
+			    }
 				//closes file
 				readF.close();
 				loop = 1;
@@ -46,8 +60,35 @@ public class dna_file {
 			}
 		}
 		System.out.println("===================================");
-		System.out.println(DNA_array);
+		//calls on valid method to check and see if it is a DNA sequence or a protein Sequence or neither
+		valid(title, DNA_array);
 
+	}
+	public static void valid(ArrayList<String> title, ArrayList<String> DNA_array) {
+		//goes through the list to check to see if protein or DNA or neither
+		for(int i = 0; i < title.size(); i++) {
+			//current title
+			String cTitle = title.get(i);
+			//current sequence
+			String cSeq = DNA_array.get(i);
+			//using regex to check and see if its a DNA
+			boolean maybeDNA = cSeq.matches("[ACGT]+");
+			//using regex to check and see if its a protein
+			boolean maybeProtein = cSeq.matches("[ACDEFGHIKLMNPQRSTVWY]+");
+			//prints out current title
+			System.out.println(cTitle);
+			//says which of the following it is
+			if(maybeDNA == true) {
+				System.out.println("Letter sequence is DNA");
+			} else if(maybeProtein ==  true) {
+				System.out.println("Letter sequence is Protein");
+			} else {
+				System.out.println("Letter sequence is invalid");
+			}
+			//prints out the DNA sequence
+			System.out.println(cSeq);
+			System.out.println();
+		}
 	}
 
 }
